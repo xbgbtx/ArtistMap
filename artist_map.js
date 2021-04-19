3fb8e776a38db132251edad469a35786c8d31d36
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-04-19 11:27:34 +0100

    Initialize page with leaflet JS map

---
+++ artist_map.js
@@ -0,0 +1,19 @@
+function page_loaded ()
+{
+    console.log ( "Artist Map starting..." );
+
+    let map = create_map ();
+}
+
+function create_map ()
+{
+    let map = L.map ("mapdiv").setView ( [51.505, -0.09], 13 );
+
+    // Add OpenStreetMap tiles
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", 
+    {
+        maxZoom: 19,
+        attribution: "&copy; <a href=\"https://openstreetmap.org/copyright\">"+
+                     "OpenStreetMap contributors</a>"
+    }).addTo(map);
+}
